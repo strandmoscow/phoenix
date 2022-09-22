4c0efc7dd8aac73ed34672a0ceb19c67761db2d8
strandmoscow <abc010801@mail.ru>
2022-09-22 23:08:23 +0300

    Flask sign in and registration pages 2

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import psycopg2
 
 app = Flask(__name__, instance_relative_config=True)
 
@@ -9,15 +10,33 @@ app.config.from_mapping(
     UPLOAD_FOLDER=UPLOAD_FOLDER
 )
 
+try:
+    conn = psycopg2.connect("dbname='Phoenix' user='net_user' host='80.211.80.219' password='net_user_password'")
+    cur = conn.cursor()
+except:
+    print("I am unable to connect to the database")
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
 
-@app.route("/registration")
+@app.route("/registration", methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    if request.method == 'POST':
+        print("!!!")
+        surname = request.form.get('inputSurname')
+        name = request.form.get('inputName')
+        email = request.form.get('inputEmail')
+        print(surname)
+        cur.execute("""SELECT version()""")
+        s = cur.fetchall()
+        print(s)
+        return render_template('register.html')
+    else:
+        print("!!!1")
+        return render_template('register.html')
 
 
 @app.route("/sign_in")
