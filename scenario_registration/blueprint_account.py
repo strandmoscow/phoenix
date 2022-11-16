b2de55e72b274ceb70c9948c5d6c21b2f99f90bf
Аnastasia <fenix2704@bk.ru>
2022-11-16 20:15:31 +0300

    new passport info on account page

--- scenario_registration/blueprint_account.py
+++ scenario_registration/blueprint_account.py
@@ -3,7 +3,8 @@ from flask import Blueprint, request, session, render_template, redirect
 account = Blueprint('account', __name__, template_folder='templates', static_folder='static')
 
 
-@account.route("/step1", methods=['GET', 'POST'])
-def step1():
+@account.route("/base_acc", methods=['GET', 'POST'])
+def base_acc():
     profile_icon = './static/abstract-user-flat-4.svg'
-    return render_template('account.html', img=profile_icon)
+    eye_icon = './static/eye.svg'
+    return render_template('account.html', img=profile_icon, eye=eye_icon)
